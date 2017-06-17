d52f899a5d995c5709706592a326e53626ec5a7a
Carson Judge <cjudge1337@gmail.com>
2017-06-17 16:10:34 -0700

    Basic cart working

--- shopping_cart/main.js
+++ shopping_cart/main.js
@@ -13,5 +13,10 @@ function addToCart() {
   let num = document.getElementById('cart-number');
   num.innerHTML = cart.length === 1 ? '1 item' : `${cart.length} items`;
 
-  
+  let dropdown = document.getElementById('dropdown-content');
+  let newItem = document.createElement('div');
+
+  newItem.className = 'dropdown-item';
+  newItem.innerHTML = event.target.dataset.item;
+  dropdown.appendChild(newItem);
 }
